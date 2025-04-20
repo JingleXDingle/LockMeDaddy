0c17fb46663aaf26cb01cae8fca4997a5af5dba4
JingleXDingle <jrubioanoceto@gmail.com>
2025-04-20 17:03:20 -0400

    Improved imput validation.

--- LockMeDaddy.py
+++ LockMeDaddy.py
@@ -1,6 +1,8 @@
-#Password Generator
+# Password Generator
 
 import random
+
+
 letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 symbols = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':', ';', '"', "'", '<', '>', '?', '/', '|']
 numbers = '0123456789'
@@ -8,9 +10,14 @@ numbers = '0123456789'
 
 # Welcomes and prompts the user for the number of characters they want in their password.
 print("Welcome to the Password Generator!")
-print("How many characters would you like in your password? ")
+print("How many letters would you like in your password? ")
 num_chars = int(input())
-
+if num_chars <= 0:
+    print("Please enter a positive number for the number of letters.")
+    exit()
+elif num_chars > 100:
+    print("Please enter a number less than or equal to 100 for the number of letters.")
+    exit()
 
 # Checks if the user wants to include symbols in the password.
 print("Would you like to include symbols? (yes/no)")
@@ -18,6 +25,9 @@ symbols_choice = input()
 if symbols_choice== 'yes':
     print("How many symbols would you like?")
     num_symbols = int(input())
+if num_symbols <= 0 or num_symbols > 50:
+    print("Please enter a number between 1 and 50 for the number of symbols.")
+    exit()
 elif symbols_choice == 'no':
     num_symbols = 0
 else: 
@@ -33,22 +43,25 @@ if numbers_choice == 'yes':
     num_numbers = int(input())
 elif numbers_choice == 'no':
     num_numbers = 0
+elif num_numbers <= 0 or num_numbers > 10:
+    print("Please enter a number between 1 and 10 for the amount of numbers.")
+    exit()    
 else: 
     print("Invalid input. Please enter 'yes' or 'no'.")
     exit()
 
 
-
+# Generates a list of random characters based on the user's input.
 password_letters= (random.choices(letters, k=num_chars))
 password_symbols= (random.choices(symbols, k=num_symbols))
 password_numbers= (random.choices(numbers, k=num_numbers))
 
-#Combines the lists of characters into one list and shuffles it.
+# Combines the lists of characters into one list and shuffles it.
 unshufled_password = password_letters + password_symbols + password_numbers
 random.shuffle(unshufled_password)
 
-#Connverts the shuffled list of characters into a string.
+# Connverts the shuffled list of characters into a string.
 Shuffled_password = ''.join(unshufled_password)
 
-#Prints the final password to the user.
+# Prints the final password to the user.
 print(f"Your password is: {Shuffled_password}") 
