c40455cf8db6cd743e2ba4590b320aaf293cb1e8
JingleXDingle <jrubioanoceto@gmail.com>
2025-04-20 12:42:09 -0400

    Added imput validation.

--- LockMeDaddy.py
+++ LockMeDaddy.py
@@ -1,27 +1,41 @@
 #Password Generator
 
 import random
-
-symbols = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':', ';', '"', "'", '<', '>', '?', '/', '|']
 letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+symbols = [ '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', ':', ';', '"', "'", '<', '>', '?', '/', '|']
 numbers = '0123456789'
 
+
+# Welcomes and prompts the user for the number of characters they want in their password.
 print("Welcome to the Password Generator!")
 print("How many characters would you like in your password? ")
 num_chars = int(input())
+
+
+# Checks if the user wants to include symbols in the password.
 print("Would you like to include symbols? (yes/no)")
 symbols_choice = input()
 if symbols_choice== 'yes':
     print("How many symbols would you like?")
     num_symbols = int(input())
-else: num_symbols = 0
+elif symbols_choice == 'no':
+    num_symbols = 0
+else: 
+    print("Invalid input. Please enter 'yes' or 'no'.")
+    exit()
 
+
+# Checks if the user wants to include numbers in the password.
 print("Would you like to include numbers? (yes/no)")
 numbers_choice = input()        
 if numbers_choice == 'yes':
     print("How many numbers would you like?")
     num_numbers = int(input())
-else: num_numbers = 0
+elif numbers_choice == 'no':
+    num_numbers = 0
+else: 
+    print("Invalid input. Please enter 'yes' or 'no'.")
+    exit()
 
 
 
